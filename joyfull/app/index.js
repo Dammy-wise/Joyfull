34867aab0fedb883f549b53ddb963021c6509f74
Coding <150249195+Dammy-wise@users.noreply.github.com>
2025-10-10 10:00:12 -0700

    1st commit

---
+++ joyfull/app/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './screens/Welcome';
+import LoginScreen from './screens/Login';
+import SignupScreen from './screens/Signup';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
